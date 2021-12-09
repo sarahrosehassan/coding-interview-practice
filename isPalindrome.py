eb1bea05f26e5d25e42e84f28ec6387b4b2fe83b
sarahrosehassan <59797227+sarahrosehassan@users.noreply.github.com>
2021-12-09 13:54:09 -0500

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -10,15 +10,12 @@ class Solution(object):
         alNumString = re.sub(r'[^A-Za-z0-9]+', '', s).lower() # remove all occurences of non-alphanumeric characters from raw-string and convert to lower case 
 
         return alNumString == alNumString[::-1] # check if string with all non-alphanumeric characters removed is the same as reversed string
-      
-      """
-      Explanation of regular expression r'[^A-Za-z0-9]+'
-      
-      ^ not
-      r raw-string to ignore escape sequences. For example, print(r'\n') prints \n not a new line
-      + to match one or more characters
-      
-      Time Complexity: O(n) (faster than 96.04 % of Python online submissions)
-      Memory Usage: 15.5 MB (less than 22.54 of Python online submissions)
-      
-      """
+    
+    """
+    Explanation of regular expression r'[^A-Za-z0-9]+'
+    ^ not
+    r raw-string to ignore escape sequences. For example, print(r'\n') prints \n not a new line
+    + to match one or more characters
+    Time Complexity: O(n) (faster than 96.04 % of Python online submissions)
+    Memory Usage: 15.5 MB (less than 22.54 of Python online submissions)
+    """
