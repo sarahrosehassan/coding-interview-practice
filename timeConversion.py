d2a036e1cabaaf8218c875e4e9e9cee51affc467
sarahrosehassan <59797227+sarahrosehassan@users.noreply.github.com>
2021-12-23 18:42:01 -0500

    Update timeConversion.py

--- timeConversion.py
+++ timeConversion.py
@@ -3,12 +3,12 @@
 
 
 def timeConversion(s):
-    if s[:2] == "12" and s[-2:] == "AM":
+    if s[:2] == "12" and s[-2:] == "AM": # if time is midnight
         return "00" + s[2:8]
 
-    elif (s[-2:] == "AM") or (s[:2] == "12" and s [-2:] == "PM"):
+    elif (s[-2:] == "AM") or (s[:2] == "12" and s [-2:] == "PM"): # if time is AM or noon
         return s[:-2]
     
         
     else:
-        return (str(int(s[:2]) + 12)) + s[2:-2]
+        return (str(int(s[:2]) + 12)) + s[2:-2] # if time is PM and not any of the above special cases
