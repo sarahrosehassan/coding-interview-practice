566bf402f4b3bd7d311d7779596de8db8c55e1fa
sarahrosehassan <59797227+sarahrosehassan@users.noreply.github.com>
2021-12-27 10:35:57 -0500

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,8 +2,8 @@
 Write a program that prints the numbers from 1 to n. 
 But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 For numbers which are multiples of both three and five print “FizzBuzz”.
-
 """
+
 def fizzBuzz(n):
     for i in range(1,n+1):
         if (i % 5) == 0 and (i % 3) == 0:
